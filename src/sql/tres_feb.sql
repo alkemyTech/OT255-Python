b3f25152cb0f4bbbab61ac56997c3f22c731fd50
Gonzalo Bossetti <gonzalo.bossetti@gmail.com>
2022-07-16 09:29:29 -0300

    PR Fixes: styles and formats

--- src/sql/tres_feb.sql
+++ src/sql/tres_feb.sql
@@ -14,11 +14,8 @@ END AS last_name,
   DATE_PART ('year', CURRENT_DATE) - DATE_PART('year', TO_DATE (ptdf.birth_dates,'YY')) AS age,
   ptdf.codigo_postal AS postal_code,
   l.localidad AS location,
-  ptdf.correos_electronicos AS email,
-FROM
-    public.palermo_tres_de_febrero ptdf LEFT JOIN public.localidad l
-ON CAST (ptdf.codigo_postal AS INTEGER) = l.codigo_postal
-WHERE 
-    universidad='universidad_nacional_de_tres_de_febrero'
+  ptdf.correos_electronicos AS email
+FROM public.palermo_tres_de_febrero ptdf LEFT JOIN public.localidad l
+    ON CAST (ptdf.codigo_postal AS INTEGER) = l.codigo_postal
+WHERE universidad='universidad_nacional_de_tres_de_febrero'
 AND fecha_de_inscripcion BETWEEN '20/Sep/01' AND '21/Feb/01'
-
--- src/sql/utn.sql
+++ src/sql/utn.sql
@@ -7,11 +7,8 @@ SELECT
   DATE_PART ('year', CURRENT_DATE) - DATE_PART('year', TO_DATE (ju.birth_date,'YYYY-MM-DD')) AS age,
   l.codigo_postal AS postal_code,
   ju.location,
-  ju.email,
-FROM
-  public.jujuy_utn ju LEFT JOIN public.localidad l
-ON ju.location = l.localidad
-WHERE 
-    university='universidad tecnológica nacional'
-AND TO_DATE(inscription_date ,'YYYY-MM-DD') BETWEEN '2020/09/01' AND '2021/02/01'
-
+  ju.email
+FROM public.jujuy_utn ju LEFT JOIN public.localidad l
+  ON ju.location = l.localidad
+WHERE university='universidad tecnológica nacional'
+  AND TO_DATE(inscription_date ,'YYYY-MM-DD') BETWEEN '2020/09/01' AND '2021/02/01'
