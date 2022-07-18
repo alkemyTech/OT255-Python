5b43be8617da65d53a094557f6823e95c0e5e1a2
Juan Cruz <ferreyra.juancruz95@gmail.com>
2022-07-18 15:20:25 -0300

    UC use array for postal_code

--- src/sql/univ_del_cine.sql
+++ src/sql/univ_del_cine.sql
@@ -12,17 +12,11 @@ SELECT
 	END as last_name,
 	lsc1.sexo as gender,
 	extract('year' from age(to_date(lsc1.birth_dates, 'DD-MM-YYYY'))) as age,
-	l1.codigo_postal as postal_code,
+	array_agg (l1.codigo_postal) as postal_code,
 	l1.localidad as location,
 	lsc1.emails as email
-FROM lat_sociales_cine lsc1 LEFT JOIN (
-	SELECT l1_temp1.localidad, l1_temp1.codigo_postal
-	FROM (
-		SELECT DISTINCT ON(localidad) localidad, codigo_postal
-		FROM localidad l1_temp2
-		ORDER BY localidad, codigo_postal
-		) as l1_temp1
-	) as l1 on REPLACE(lsc1.locations, '-', ' ') = l1.localidad
+FROM lat_sociales_cine lsc1 LEFT JOIN localidad l1 on REPLACE(lsc1.locations, '-', ' ') = l1.localidad
 WHERE lsc1.universities = 'UNIVERSIDAD-DEL-CINE'
 AND to_date(lsc1.inscription_dates, 'DD-MM-YYYY') BETWEEN '2020-09-01' AND '2021-02-01'
+GROUP BY lsc1.id, l1.localidad
 ORDER BY lsc1.id;
