266afa2156e7a7ba4e65f51923d4f163636490e3
Gonzalo Bossetti <gonzalo.bossetti@gmail.com>
2022-07-19 13:48:18 -0300

    NULL CP data, fixed up. No duplicated value

--- src/sql/utn.sql
+++ src/sql/utn.sql
@@ -5,10 +5,14 @@ SELECT
   SPLIT_PART(ju.nombre, ' ', 1) AS first_name, SPLIT_PART(ju.nombre, ' ', 2) AS last_name,
   ju.sexo AS gender,
   EXTRACT(year from AGE(CURRENT_DATE, TO_DATE (ju.birth_date,'YYYY-MM-DD'))) AS age,
-  l.codigo_postal AS postal_code,
-  ju.location,
+  array_agg(l.codigo_postal) as postal_code,
+  location,
   ju.email
-FROM public.jujuy_utn ju LEFT JOIN public.localidad l
-  ON ju.location = l.localidad
-WHERE university='universidad tecnológica nacional'
+FROM 
+  public.jujuy_utn ju
+LEFT JOIN public.localidad l
+  ON ju.location = LOWER(l.localidad)
+  WHERE university='universidad tecnológica nacional'
   AND TO_DATE(inscription_date ,'YYYY-MM-DD') BETWEEN '2020/09/01' AND '2021/02/01'
+GROUP BY 
+  id
