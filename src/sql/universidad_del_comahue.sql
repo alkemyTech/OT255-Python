fd9bfc477310b6fe64910a0cfc37f7da63682e5b
Exhn <100232788+Exhn@users.noreply.github.com>
2022-07-20 20:29:45 -0300

    Queries sql univ comahue salvador (#19)
    * Creación de nueva branch
    Se suben archivos de sql de ambas universidades
    
    * Modificación de nombre de branch
    
    * Se modifican archivos sql de acuerdo a
    correciones
    
    * Delete .idea directory
    
    Borrando archivos adicionales
    
    * Se configura DAG con retries

---
+++ src/sql/universidad_del_comahue.sql
@@ -0,0 +1,24 @@
+select
+universidad as university,
+carrera as career,
+fecha_de_inscripcion as inscription_date,
+CASE
+      WHEN split_part(name::text, ' ',1) in ('MR.','DR.', 'MS.','MRS.')  THEN split_part(name::text, ' ',2)
+ELSE split_part(name::text, ' ',1)
+end as first_name,
+CASE
+      WHEN split_part(name::text, ' ',1) in ('MR.','DR.', 'MS.', 'MRS.')  THEN split_part(name::text, ' ',3)
+ELSE split_part(name::text, ' ',2)
+end as last_name,
+sexo as gender,
+extract (year from age (now()::date,fecha_nacimiento::date))   as age,
+fc.codigo_postal as postal_code,
+disloc.localidad as location,
+correo_electronico as email
+from flores_comahue fc
+join (
+select * from localidad loc
+) disloc
+on disloc.codigo_postal = cast(fc.codigo_postal as integer)
+where fc.universidad = 'UNIV. NACIONAL DEL COMAHUE'
+and fecha_de_inscripcion::date between '2020-09-01' and '2021-02-01'
