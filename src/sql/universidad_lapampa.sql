50adeaf6fec70e0122b35678018fe15680141b5d
Lucas Fernandez Santos <89107891+LucasFerSantos@users.noreply.github.com>
2022-07-20 20:10:54 -0300

    bases sql la pampa e interamericana
    * bases sql la pampa e interamericana
    
    * first dags
    
    * sql modificados

---
+++ src/sql/universidad_lapampa.sql
@@ -0,0 +1,19 @@
+SELECT universidad AS university,
+carrerra AS carrer,
+to_date(fechaiscripccion,'DD-MM-YYYY') AS inscription_date,
+CASE
+	WHEN split_part(nombrre::text,' ',1) in ('Mr.','Dr.','Ms.','Mrs.') THEN split_part(nombrre::text,' ',2)
+ELSE split_part(nombrre::text,' ',1) end AS first_name,
+CASE
+	WHEN split_part(nombrre::text,' ',1) in ('Mr.','Dr.','Ms.','Mrs.') THEN split_part(nombrre::text,' ',3)
+ELSE split_part(nombrre::text,' ',2) end AS last_name,
+sexo AS gender,
+extract('year' from age(to_date(nacimiento, 'DD-MM-YYYY'))) AS age,
+CAST (lp.codgoposstal AS int) AS postal_code,
+localidad AS location,
+eemail AS email
+
+FROM   public.moron_nacional_pampa lp
+LEFT JOIN localidad l ON CAST (lp.codgoposstal AS int) = l.codigo_postal
+WHERE universidad = 'Universidad nacional de la pampa' 
+AND to_date(fechaiscripccion, 'DD-MM-YYYY') BETWEEN '2020-09-01' AND '2021-02-01';
