912675bcdc9bf1a3f0d989aeecf3e7a46e9cc504
Gonzalo Bossetti <gonzalo.bossetti@gmail.com>
2022-07-20 09:54:12 -0300

    Arranges after consulting with menthor

--- airflow/dags/dag_utn.py
+++ airflow/dags/dag_utn.py
@@ -1,47 +1,57 @@
 from airflow import DAG
-from airflow.providers.postgres.operators.postgres import PostgresOperator
-from airflow.providers.postgres.operators.postgres import PostgresHook
+#from airflow.providers.postgres.operators.postgres import PostgresOperator
+#from airflow.providers.postgres.operators.postgres import PostgresHook
+from airflow.providers.amazon.aws.operators.s3 import S3CreateBucketOperator
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.operators.python import PythonOperator
 
 from datetime import timedelta, datetime
 
+def database_connect():
+    pass
+
+def export_data():
+    pass
 
 def data_transform():
     pass
 
-def hook_and_upload_to_s3():
+def hook_and_upload():
     # hook = S3Hook(" ")      # [Connection_Id_name]
     # hook.load_file(" ")     # [file_name]
     pass
 
 with DAG(
-    'utn_table_data',
+    '3feb_table_data',
     description = 'Conection with utn data in training db',
     start_date = datetime(2022, 7, 19),
     schedule_interval = timedelta(hours = 1)
 ) as dag:
     
-    task_1 = PostgresHook (
+    t_conect_db = PythonOperator (
         task_id = 'db_connect',
-        # postgress_conn_id = " ",         # The postgress conn id reference to a specific postgres database
-        retries = 5,
-
+        python_callable = database_connect,
+        retries = 5
     )
 
-    task_2 = PostgresOperator(
-        task_id = 'utn_data_consult',
-        # postgres_conn_id = " ",         # [Connection_Id_name]
-        # sql = " "                       # [sql_consult_name_file.sql]
+    t_export_data = PythonOperator(
+        task_id = 'export_data',
+        python_callable = export_data
     )
 
-    task_3 = PythonOperator(
+    t_data_transform = PythonOperator(
         task_id = 'data_transform',
         python_callable = data_transform
     )
 
-    task_4 = PythonOperator(
-        task_id = "hook_and_upload_to_s3"    
+    t_creat_bucket = S3CreateBucketOperator(
+        task_id = 'create_bucket',
+        bucket_name = 'univ_3febrero_bucket'
     )
 
-    task_1 >> task_2 >> task_3 >> task_4
+    t_data_load = PythonOperator(
+        task_id = "hook_and_upload_to_s3",
+        python_callable = hook_and_upload
+    )
+    
+    t_conect_db >> t_export_data >> t_data_transform >> t_creat_bucket >> t_data_load
