bd9d5524f6bd0aedcfb906e057460421e0211718
Juan Cruz <ferreyra.juancruz95@gmail.com>
2022-07-21 13:39:25 -0300

    UC-UBA replace dashes in query result

---
+++ src/sql/univ_buenos_aires.sql
@@ -0,0 +1,22 @@
+SELECT
+	replace(uk.universidades, '-', ' ') as university,
+	replace(uk.carreras, '-', ' ') as career,
+	to_date(uk.fechas_de_inscripcion, 'YY-MON-DD') as inscription_date,
+	CASE 
+		WHEN split_part(uk.nombres, '-', 1) in ('DR.', 'DR.', 'MRS.', 'MS.') THEN split_part(uk.nombres, '-', 2)
+		ELSE split_part(uk.nombres, '-', 1)
+	END as first_name, -- select for first name: second name if preffix / first name if no preffix
+	CASE 
+		WHEN split_part(uk.nombres, '-', 1) in ('DR.', 'DR.', 'MRS.', 'MS.') THEN split_part(uk.nombres, '-', 3)
+		ELSE split_part(uk.nombres, '-', 2)
+	END as last_name, -- select for last name: third name if preffix / second name if no preffix
+	uk.sexo as gender,
+	extract('year' from age(to_date(uk.fechas_nacimiento, 'YY-MON-DD'))) as age, -- calculate age from birth date
+	l1.codigo_postal as postal_code,
+	l1.localidad as location,
+	uk.emails as email
+FROM uba_kenedy uk LEFT JOIN localidad as l1 on cast(uk.codigos_postales as int) = l1.codigo_postal -- join localidad to get locations
+WHERE uk.universidades = 'universidad-de-buenos-aires' -- filter by university
+	AND to_date(uk.fechas_de_inscripcion, 'YY-MON-DD') BETWEEN '2020-09-01' AND '2021-02-01' -- filer by incription date
+ORDER BY uk.id -- order by original id to compare original table and query result
+;
---
+++ src/sql/univ_del_cine.sql
@@ -0,0 +1,23 @@
+SELECT
+	replace(lsc1.universities, '-', ' ') as university,
+	replace(lsc1.careers, '-', ' ') as career,
+	to_date(lsc1.inscription_dates, 'DD-MM-YYYY') as inscription_date,
+	CASE 
+		WHEN split_part(lsc1.names, '-', 1) IN ('DR.', 'DR.', 'MRS.', 'MS.') THEN split_part(lsc1.names, '-', 2)
+		ELSE split_part(lsc1.names, '-', 1)
+	END as first_name, -- select for first name: second name if preffix / first name if no preffix
+	CASE 
+		WHEN split_part(lsc1.names, '-', 1) IN ('DR.', 'DR.', 'MRS.', 'MS.') THEN split_part(lsc1.names, '-', 3)
+		ELSE split_part(lsc1.names, '-', 2)
+	END as last_name, -- select for last name: third name if preffix / second name if no preffix
+	lsc1.sexo as gender,
+	extract('year' from age(to_date(lsc1.birth_dates, 'DD-MM-YYYY'))) as age, -- calculate age from birth date
+	array_agg (l1.codigo_postal) as postal_code, -- select possible postal codes inside an array to avoid duplicated results
+	l1.localidad as location,
+	lsc1.emails as email
+FROM lat_sociales_cine lsc1 LEFT JOIN localidad l1 on replace(lsc1.locations, '-', ' ') = l1.localidad -- join localidad to get postal codes
+WHERE lsc1.universities = 'UNIVERSIDAD-DEL-CINE' -- filter by university
+AND to_date(lsc1.inscription_dates, 'DD-MM-YYYY') BETWEEN '2020-09-01' AND '2021-02-01' -- filer by incription date
+GROUP BY lsc1.id, l1.localidad -- group by id and localidad to generate the array of possible postal codes
+ORDER BY lsc1.id -- order by original id to compare original table and query result
+;
