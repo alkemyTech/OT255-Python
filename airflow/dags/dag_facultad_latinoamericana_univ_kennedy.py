e5a64d498b69d62866e8404f939070dabc7245d2
Adrian Pinero <adrianucv@gmail.com>
2022-07-21 15:28:20 -0400

    Corregido los paquetes importados que no se utilizaban.

--- airflow/dags/dag_facultad_latinoamericana_univ_kennedy.py
+++ airflow/dags/dag_facultad_latinoamericana_univ_kennedy.py
@@ -1,18 +1,20 @@
-from airflow import DAG
-from airflow.operators.python import PythonOperator
+import logging
 from datetime import datetime, timedelta
 
-import logging
+from airflow import DAG
+from airflow.operators.python import PythonOperator
 
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s %(levelname)s %(message)s',
-    datefmt='%Y-%m-%d')
+    format="%(asctime)s %(levelname)s %(message)s",
+    datefmt="%Y-%m-%d",
+)
 
 
 def extraer_latinoam():
     logging.info(
-        "Extrayendo datos de la Facultad Latinoamericana de Ciencias Sociales")
+        "Extrayendo datos de la Facultad Latinoamericana de Ciencias Sociales"
+    )
     pass
 
 
@@ -33,42 +35,47 @@ def load_s3():
 
 def log_inicio():
     logging.info(
-        'Iniciando el dag: dag_facultad_latinoamericana_universidad_kennedy')
+        "Iniciando el dag: dag_facultad_latinoamericana_universidad_kennedy"
+    )
 
 
 with DAG(
-    'dag_facultad_latinoamericana_universidad_kennedy',
-    description='Operators que se deberán utilizar a futuro',
+    "dag_facultad_latinoamericana_universidad_kennedy",
+    description="Operators que se deberán utilizar a futuro",
     schedule_interval=timedelta(hours=1),
-    start_date=datetime(2022, 7, 19)
+    start_date=datetime(2022, 7, 19),
 ) as dag:
 
     # Log de inicio del DAG
     log_inicio_dag = PythonOperator(
-        task_id='log_inicio',
-        python_callable=log_inicio
+        task_id="log_inicio", python_callable=log_inicio
     )
 
     # Extraer la informacion de las dos universidades
     extraer_fac_latinoamericana = PythonOperator(
-        task_id='extraer_fac_latinoamericana',
+        task_id="extraer_fac_latinoamericana",
         python_callable=extraer_latinoam,
         retries=5,
-        retry_delay=timedelta(seconds=120)
+        retry_delay=timedelta(seconds=120),
     )
     extraer_kennedy = PythonOperator(
-        task_id='extraer_kennedy',
+        task_id="extraer_kennedy",
         python_callable=extraer_kenn,
         retries=5,
-        retry_delay=timedelta(seconds=120)
+        retry_delay=timedelta(seconds=120),
     )
 
-# Transformar la data usando pandas
+    # Transformar la data usando pandas
     transform = PythonOperator(
-        task_id='transform', python_callable=transform_data)
+        task_id="transform", python_callable=transform_data
+    )
 
-# Carga el archivo en el servidor s3
-    load_to_s3 = PythonOperator(task_id='load_s3', python_callable=load_s3)
+    # Carga el archivo en el servidor s3
+    load_to_s3 = PythonOperator(task_id="load_s3", python_callable=load_s3)
 
-    log_inicio_dag >> [extraer_fac_latinoamericana,
-                       extraer_kennedy] >> transform >> load_to_s3
+    (
+        log_inicio_dag
+        >> [extraer_fac_latinoamericana, extraer_kennedy]
+        >> transform
+        >> load_to_s3
+    )
