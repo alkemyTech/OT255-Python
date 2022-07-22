0ecfe0e4c44d8a7228254b1c275bd43ed0f2cebb
AdrianPinero <102536977+AdrianPinero@users.noreply.github.com>
2022-07-22 15:43:08 +0200

    Dag facultad latinoamericana y kenedy
    * Agregado el query de la facultad latinoamericana de ciencias sociales
    
    * Agregado los query de la facultad latinoamericana de ciencias sociales y la universidad J.F. Kenedy
    
    * Corregidas las consultas de la universidad J.F. Kennedy y la Facultad Latinoamericana.
    
    * Agregado el dag de la faultad latinoamericana y de la universidad J.F. Kennedy
    
    * Corregidos las dos consultas de SQL
    
    * Corregido los paquetes importados que no se utilizaban.
    
    * Corregido los paquetes importados que no se utilizaban.

---
+++ airflow/dags/dag_facultad_latinoamericana_univ_kennedy.py
@@ -0,0 +1,81 @@
+import logging
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(message)s",
+    datefmt="%Y-%m-%d",
+)
+
+
+def extraer_latinoam():
+    logging.info(
+        "Extrayendo datos de la Facultad Latinoamericana de Ciencias Sociales"
+    )
+    pass
+
+
+def extraer_kenn():
+    logging.info("Extrayendo datos de la Universidad J.F. Kennedy")
+    pass
+
+
+def transform_data():
+    logging.info("Transformando la data")
+    pass
+
+
+def load_s3():
+    logging.info("Cargando la data a S3")
+    pass
+
+
+def log_inicio():
+    logging.info(
+        "Iniciando el dag: dag_facultad_latinoamericana_universidad_kennedy"
+    )
+
+
+with DAG(
+    "dag_facultad_latinoamericana_universidad_kennedy",
+    description="Operators que se deberán utilizar a futuro",
+    schedule_interval=timedelta(hours=1),
+    start_date=datetime(2022, 7, 19),
+) as dag:
+
+    # Log de inicio del DAG
+    log_inicio_dag = PythonOperator(
+        task_id="log_inicio", python_callable=log_inicio
+    )
+
+    # Extraer la informacion de las dos universidades
+    extraer_fac_latinoamericana = PythonOperator(
+        task_id="extraer_fac_latinoamericana",
+        python_callable=extraer_latinoam,
+        retries=5,
+        retry_delay=timedelta(seconds=120),
+    )
+    extraer_kennedy = PythonOperator(
+        task_id="extraer_kennedy",
+        python_callable=extraer_kenn,
+        retries=5,
+        retry_delay=timedelta(seconds=120),
+    )
+
+    # Transformar la data usando pandas
+    transform = PythonOperator(
+        task_id="transform", python_callable=transform_data
+    )
+
+    # Carga el archivo en el servidor s3
+    load_to_s3 = PythonOperator(task_id="load_s3", python_callable=load_s3)
+
+    (
+        log_inicio_dag
+        >> [extraer_fac_latinoamericana, extraer_kennedy]
+        >> transform
+        >> load_to_s3
+    )
---
+++ src/sql/query_facultad_latinoamericana_de_ciencias_sociales.sql
@@ -0,0 +1,26 @@
+SELECT universities AS university,
+careers AS career,
+to_date(inscription_dates, 'DD-MM-YYYY') as inscription_date,
+CASE
+		WHEN SPLIT_PART(names, '-', 1) IN ('MRS.', 'MISS', 'MR.') THEN SPLIT_PART(names, '-', 2)
+ELSE SPLIT_PART(names, '-', 1)
+END AS first_name,
+CASE
+		WHEN SPLIT_PART(names, '-', 1) IN ('MRS.', 'MISS', 'MR.') THEN SPLIT_PART(names, '-', 3)
+ELSE SPLIT_PART(names, '-', 2)
+END AS last_name,
+sexo AS gender,
+DATE_PART('year', now()::date) - DATE_PART('year', to_date(birth_dates, 'DD-MM-YYYY')::date) AS age,
+localidad2.codigo_postal AS postal_code,
+locations AS location,
+emails AS email
+FROM lat_sociales_cine 
+LEFT JOIN (SELECT localidad, 
+		   ARRAY_AGG(codigo_postal ORDER BY codigo_postal) codigo_postal 
+		   FROM localidad 
+		   GROUP BY localidad
+		   ) 
+		   AS localidad2
+ON lat_sociales_cine.locations = replace(localidad2.localidad, ' ', '-')
+WHERE universities='-FACULTAD-LATINOAMERICANA-DE-CIENCIAS-SOCIALES'
+AND to_date(inscription_dates, 'DD-MM-YYYY') BETWEEN '2020-09-01' AND '2021-02-01'
