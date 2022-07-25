a638bd64d23cf7471b925e2edd13b23c08bef68f
Juan Cruz <ferreyra.juancruz95@gmail.com>
2022-07-25 20:00:31 -0300

    UC finish task for queries

--- src/py_functions/query_uc.py
+++ src/py_functions/query_uc.py
@@ -1,13 +1,24 @@
-from dataclasses import replace
-from dotenv import load_dotenv
+import datetime
 import os
-from sqlalchemy import create_engine
-import pandas as pd
-from pathlib import Path
+import shelve
 import sys
+from pathlib import Path
+
+import pandas as pd
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
 
 os.chdir(Path(sys.path[0]) / "../..")
-print(Path.cwd())
+
+if not os.path.isdir(Path.cwd() / "files/raw"):
+    os.makedirs(Path.cwd() / "files/raw")
+if not os.path.isdir(Path.cwd() / "files/modified"):
+    os.makedirs(Path.cwd() / "files/modified")
+if not os.path.isdir(Path.cwd() / "files/temp"):
+    os.makedirs(Path.cwd() / "files/temp")
+
+shelf_file = shelve.open("files/temp/last_file")
+univ_name = "uc"
 
 load_dotenv()
 
@@ -17,13 +28,18 @@ db_name = os.getenv("DB_NAME")
 db_user = os.getenv("DB_USER")
 db_pass = os.getenv("DB_PASS")
 
-uc_query = open("src/sql/query_uc.sql", "r")
+query = open(f"src/sql/query_{univ_name}.sql", "r")
 
 engine = create_engine(
     f"postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}"
 )
 
-df_uc = pd.read_sql_query(uc_query.read(), engine)
-uc_query.close()
+df_query = pd.read_sql_query(query.read(), engine)
 
-df_uc.to_csv("files/raw/raw_uc.csv", replace=True)
+today = datetime.datetime.now()
+
+file_name = "_".join(
+    ["-".join([str(today.year), str(today.month).zfill(2), str(today.day).zfill(2)]), univ_name]
+)
+
+df_query.to_csv(f"files/raw/{file_name}.csv", index=False)
