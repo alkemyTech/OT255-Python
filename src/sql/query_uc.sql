e7a13dbbc41a33d76dc260965ec3ce6ce5c3d9ba
Juan Cruz <ferreyra.juancruz95@gmail.com>
2022-07-26 16:13:33 -0300

    UC-UBA edit query output (delete join with locations)

--- src/sql/query_uc.sql
+++ src/sql/query_uc.sql
@@ -1,8 +1,8 @@
-WITH localidad_t AS (
-	SELECT l2.localidad, array_agg(l2.codigo_postal) as codigo_postal
-	FROM localidad l2
-	GROUP BY l2.localidad
-	)
+--WITH localidad_t AS (
+--	SELECT l2.localidad, array_agg(l2.codigo_postal) as codigo_postal
+--	FROM localidad l2
+--	GROUP BY l2.localidad
+--	)
 SELECT
 	replace(lsc1.universities, '-', ' ') as university,
 	replace(lsc1.careers, '-', ' ') as career,
@@ -17,10 +17,11 @@ SELECT
 	END as last_name, -- select for last name: third name if preffix / second name if no preffix
 	lsc1.sexo as gender,
 	extract('year' from age(to_date(lsc1.birth_dates, 'DD-MM-YYYY'))) as age, -- calculate age from birth date
-	l1.codigo_postal as postal_code, -- select possible postal codes inside an array to avoid duplicated results
-	l1.localidad as location,
+	replace(lsc1.locations, '-', ' ') as location,
+--	l1.codigo_postal as postal_code, -- select possible postal codes inside an array to avoid duplicated results
+--	l1.localidad as location,
 	lsc1.emails as email
-FROM lat_sociales_cine lsc1 LEFT JOIN localidad_t l1 on replace(lsc1.locations, '-', ' ') = l1.localidad -- join localidad to get postal codes
+FROM lat_sociales_cine lsc1 -- LEFT JOIN localidad_t l1 on replace(lsc1.locations, '-', ' ') = l1.localidad -- join localidad to get postal codes
 WHERE lsc1.universities = 'UNIVERSIDAD-DEL-CINE' -- filter by university
 AND to_date(lsc1.inscription_dates, 'DD-MM-YYYY') BETWEEN '2020-09-01' AND '2021-02-01' -- filer by incription date
 ORDER BY lsc1.id -- order by original id to compare original table and query result
