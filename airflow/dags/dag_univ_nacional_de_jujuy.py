51169a645f5a6416e1e368dda57503cf01139f7b
nadia <nadia@debian-BULLSEYE-live-builder-AMD64>
2022-07-26 03:02:48 -0300

    dags de univ. nac. de Jujuy y univ. de Palermo

---
+++ airflow/dags/dag_univ_nacional_de_jujuy.py
@@ -0,0 +1,58 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+import logging
+
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(message)s",
+    datefmt="%Y-%m-%d"
+)
+
+args = {
+	'description': 'obtener información sobre Univ. Nacional de Jujuy',
+    'start_date': datetime(2022, 7, 30)
+}
+
+dag = DAG('dag_univ_nac_de_jujuy',default_args=args)
+
+def extraer_datos_univ_nac_de_jujuy():
+    pass
+
+def transformar_datos_univ_nac_de_jujuy():
+    pass
+
+def cargar_datos_univ_nac_de_jujuy():
+    pass
+
+
+extraer_datos_univ_nac_de_jujuy = PythonOperator(
+    task_id = 'extraer_datos',
+    retries = 5,
+    retry_delay = timedelta(minutes=1),
+    python_callable = extraer_datos_univ_nac_de_jujuy,
+    dag=dag
+)
+
+obtener_datos_univ_nac_de_jujuy = PostgresOperator(
+    task_id = 'extraer_datos',
+    retries = 5,
+    retry_delay = timedelta(minutes=1),
+    sql='proyecto_1/sql/univ_nacional_de_jujuy.sql',
+    dag=dag
+)
+
+transformar_datos_univ_nac_de_jujuy = PythonOperator(
+    task_id = 'transformar_datos',
+    python_callable = transformar_datos_univ_nac_de_jujuy,
+    dag=dag
+)
+
+cargar_datos_univ_nac_de_jujuy = PythonOperator(
+    task_id = "cargar_datos",
+    python_callable = cargar_datos_univ_nac_de_jujuy,
+    dag=dag
+)
+
+extraer_datos_univ_nac_de_jujuy << transformar_datos_univ_nac_de_jujuy << cargar_datos_univ_nac_de_jujuy
---
+++ airflow/dags/dag_univ_palermo.py
@@ -0,0 +1,48 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+import logging
+
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(message)s",
+    datefmt="%Y-%m-%d"
+)
+
+args = {
+	'description': 'obtener información sobre Univ. de Palermo',
+    'start_date': datetime(2022, 7, 30)
+}
+
+dag = DAG('dag_univ_palermo',default_args=args)
+
+def extraer_datos_univ_palermo():
+    pass
+
+def transformar_datos_univ_palermo():
+    pass
+
+def cargar_datos_univ_palermo():
+    pass
+
+extraer_datos_univ_palermo = PythonOperator(
+    task_id = 'extraer_datos',
+    retries = 5,
+    retry_delay = timedelta(minutes=1),
+    python_callable = extraer_datos_univ_palermo,
+    dag=dag
+)
+
+transformar_datos_univ_palermo = PythonOperator(
+    task_id = 'transformar_datos',
+    python_callable = transformar_datos_univ_palermo,
+    dag=dag
+)
+
+cargar_datos_univ_palermo = PythonOperator(
+    task_id = "cargar_datos",
+    python_callable = cargar_datos_univ_palermo,
+    dag=dag
+)
+
+extraer_datos_univ_palermo << transformar_datos_univ_palermo << cargar_datos_univ_palermo
