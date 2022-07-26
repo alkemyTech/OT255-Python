223f720c90d0a3bea9af258e70079b673e03fd1f
Juan Cruz <ferreyra.juancruz95@gmail.com>
2022-07-26 16:13:33 -0300

    UC-UBA edit query output (delete join with locations)

--- src/sql/query_uba.sql
+++ src/sql/query_uba.sql
@@ -12,10 +12,11 @@ SELECT
 	END as last_name, -- select for last name: third name if preffix / second name if no preffix
 	uk.sexo as gender,
 	extract('year' from age(to_date(uk.fechas_nacimiento, 'YY-MON-DD'))) as age, -- calculate age from birth date
-	l1.codigo_postal as postal_code,
-	l1.localidad as location,
+	cast(uk.codigos_postales as int) as postal_code,
+--	l1.codigo_postal as postal_code,
+--	l1.localidad as location,
 	uk.emails as email
-FROM uba_kenedy uk LEFT JOIN localidad as l1 on cast(uk.codigos_postales as int) = l1.codigo_postal -- join localidad to get locations
+FROM uba_kenedy uk -- LEFT JOIN localidad as l1 on cast(uk.codigos_postales as int) = l1.codigo_postal -- join localidad to get locations
 WHERE uk.universidades = 'universidad-de-buenos-aires' -- filter by university
 	AND to_date(uk.fechas_de_inscripcion, 'YY-MON-DD') BETWEEN '2020-09-01' AND '2021-02-01' -- filer by incription date
 ORDER BY uk.id -- order by original id to compare original table and query result
