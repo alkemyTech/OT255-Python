1ad4731f0dd0fef4dcd922d238a573bf9c4cc271
Juan Cruz <ferreyra.juancruz95@gmail.com>
2022-07-26 22:30:54 -0300

    UC-UBA combine tasks for each university in one script

--- src/py_functions/process_univ.py
+++ src/py_functions/process_univ.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def process_univ(univ):
     import os
     import shelve
@@ -6,12 +7,28 @@ def process_univ(univ):
     import pandas as pd
     from pandas.api.types import is_string_dtype
 
+=======
+import os
+import shelve
+import sys
+from pathlib import Path
+
+import pandas as pd
+from pandas.api.types import is_string_dtype
+
+
+def main(univ: str):
+>>>>>>> ccb5a7c (UC-UBA combine tasks for each university in one script)
     assert isinstance(univ, str), "University acronym must be string type."
 
     # -- FUNCTIONS --
     # change column dtype for columns present in the current dataframe
     def column_dtype_changer(df):
+<<<<<<< HEAD
         dtype_dict = {
+=======
+        dtype_Dict = {
+>>>>>>> ccb5a7c (UC-UBA combine tasks for each university in one script)
             "university": str,
             "career": str,
             "inscription_date": str,
@@ -25,13 +42,18 @@ def process_univ(univ):
         }
 
         for column in df:
+<<<<<<< HEAD
             df[column] = df[column].apply(dtype_dict[column])
+=======
+            df[column] = df[column].apply(dtype_Dict[column])
+>>>>>>> ccb5a7c (UC-UBA combine tasks for each university in one script)
         return df
 
     # change format for columns with str dtype in the current dataframe
     def string_column_formatter(df):
         for column in df:
             if is_string_dtype(df[column]):
+<<<<<<< HEAD
                 # delete hyphens except in 'inscription_date' column
                 if column != "inscription_date":
                     df[column].replace("-", " ", regex=True, inplace=True)
@@ -41,6 +63,27 @@ def process_univ(univ):
         return df
 
     # -- INITIAL CONFIG --
+=======
+                df[column] = df[column].map(border_blank_deleter)
+                df[column] = df[column].map(str.lower)
+                # delete hyphens except in 'inscription_date' column
+                if column != "inscription_date":
+                    df[column].replace("-", " ", regex=True, inplace=True)
+        return df
+
+    # delete unexpected spaces for current dataframe values
+    def border_blank_deleter(x):
+        if x[0].isspace():
+            x = x[1:]
+        if x[-1].isspace():
+            x = x[:-1]
+        return x
+
+    # -- INITIAL CONFIG --
+    # set the repository main folder as cwd
+    os.chdir(Path(sys.path[0]) / "../..")
+
+>>>>>>> ccb5a7c (UC-UBA combine tasks for each university in one script)
     # set the name of the university as a variable to simplify code reuse.
     univ_name = univ
     # set the origin and destination paths as a variable to simplify code reuse.
@@ -73,8 +116,13 @@ def process_univ(univ):
     url = "https://drive.google.com/uc?id=" + url.split("/")[-2]
     df_location = pd.read_csv(url, index_col=False)
     # rename columns to simplify future merging with university dataframe.
+<<<<<<< HEAD
     rename_dict = {"codigo_postal": "postal_code", "localidad": "location"}
     df_location = df_location.rename(columns=rename_dict)
+=======
+    rename_Dict = {"codigo_postal": "postal_code", "localidad": "location"}
+    df_location = df_location.rename(columns=rename_Dict)
+>>>>>>> ccb5a7c (UC-UBA combine tasks for each university in one script)
     # change columns dtype to match expected output.
     column_dtype_changer(df_location)
     # format columns with string dtype to match expected output.
@@ -122,4 +170,10 @@ def process_univ(univ):
     # - export resulting dataframe -
     df_univ.to_csv(modified_path / "".join([file_name, ".csv"]), index=False)
 
+<<<<<<< HEAD
     print("Query task finished")
+=======
+
+if __name__ == "__main__":
+    main()
+>>>>>>> ccb5a7c (UC-UBA combine tasks for each university in one script)
