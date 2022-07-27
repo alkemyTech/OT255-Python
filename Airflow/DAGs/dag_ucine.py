95de8fe52bcf0c7b0ff0849d7505f4cd6379b124
Juan Cruz <ferreyra.juancruz95@gmail.com>
2022-07-26 23:08:42 -0300

    UC-UBA load scripts as modules for DAG tasks

--- Airflow/DAGs/dag_ucine.py
+++ Airflow/DAGs/dag_ucine.py
@@ -1,4 +1,5 @@
 import logging
+<<<<<<< HEAD
 from datetime import datetime, timedelta
 
 from airflow import DAG
@@ -8,6 +9,20 @@ from airflow.providers.amazon.aws.transfers.local_to_s3 import \
 
 from include.process_univ import process_univ
 from include.query_univ import query_univ
+=======
+import sys
+from datetime import datetime, timedelta
+from pathlib import Path
+
+from airflow.operators.python import PythonOperator
+
+from airflow import DAG
+
+path_root = Path(__file__).parents[2]
+sys.path.append(str(path_root))
+
+from src.py_functions import process_univ, query_univ
+>>>>>>> d60e4a4 (UC-UBA load scripts as modules for DAG tasks)
 
 # -- INITIAL CONFIG --
 # set the name of the university as a variable to simplify code reuse.
@@ -38,15 +53,22 @@ with DAG(
     catchup=False,
 ) as dag:
     # first task: run sql script and export query result to (.csv)?
+<<<<<<< HEAD
     task_query_univ = PythonVirtualenvOperator(
         task_id=f"task_query_{univ_name}",
         python_callable=query_univ,
         op_args=[univ_name],
         requirements=["pandas", "python-dotenv", "sqlalchemy", "psycopg2-binary"],
+=======
+    task_query_univ = PythonOperator(
+        task_id=f"task_query_{univ_name}",
+        python_callable=query_univ(univ_name),
+>>>>>>> d60e4a4 (UC-UBA load scripts as modules for DAG tasks)
         retries=5,
         retry_delay=timedelta(minutes=2),
     )
     # second task: process raw data in pandas
+<<<<<<< HEAD
     task_process_univ = PythonVirtualenvOperator(
         task_id=f"task_process_{univ_name}",
         python_callable=process_univ,
@@ -62,5 +84,12 @@ with DAG(
         dest_bucket="cohorte-julio-8972766c",
         replace=True,
     )
+=======
+    task_process_univ = PythonOperator(
+        task_id=f"task_process_{univ_name}", python_callable=process_univ(univ_name)
+    )
+    # third task: upload resulting object to amazon s3
+    task_load_univ = PythonOperator(task_id=f"task_load_{univ_name}", python_callable=None)
+>>>>>>> d60e4a4 (UC-UBA load scripts as modules for DAG tasks)
 
 task_query_univ >> task_process_univ >> task_load_univ
