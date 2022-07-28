901195f91aac5e0edb3764b756c148b576447c0f
VladimirMorote <vldmrmorote@gmail.com>
2022-07-28 13:05:48 -0300

    sql universidad de palermo

--- SQL/sql_univ_palermo.sql
+++ SQL/sql_univ_palermo.sql
@@ -1,15 +1,33 @@
 
-SELECT universidad as university,
-    careers as career,
-    fecha_de_inscripcion as inscription_date,
-    "names" as "name",
-    sexo as gender,
-    birth_dates as age,
-    codigo_postal as postal_code,
-    direcciones as "location",
-    correos_electronicos as email
-FROM palermo_tres_de_febrero
+SELECT universidad AS university,
+careers AS carrer,
+to_date(fecha_de_inscripcion, 'YY-MON-DD') AS inscription_date,
+CASE
+	WHEN split_part(names::text,'-',1) in ('Mr.','Dr.','Ms.','Mrs.') THEN split_part(names::text,'-',2)
+ELSE split_part(names::text,'-',1) end AS first_name,
+CASE
+	WHEN split_part(names::text,'-',1) in ('Mr.','Dr.','Ms.','Mrs.') THEN split_part(names::text,'-',3)
+ELSE split_part(names::text,'-',2) end AS last_name,
+sexo AS gender,
+extract('year' from age(to_date(birth_dates, 'YY-MON-DD'))) AS age,
+array_agg(l.codigo_postal ) AS postal_code,
+CAST(UPPER(REPLACE (l.localidad,'-',' ')) AS character varying) AS location,
+correos_electronicos AS email
+
+FROM  public.palermo_tres_de_febrero ui
+LEFT JOIN localidad l ON  CAST (ui.codigo_postal AS int)= l.codigo_postal
+
 WHERE universidad = '_universidad_de_palermo'
-    AND date(fecha_de_inscripcion) >= '01/Sep/20'
-    AND date(fecha_de_inscripcion) <= '01/Feb/21'
+and to_date(fecha_de_inscripcion, 'DD-MON-YYYY') BETWEEN ' 01-09-2020' AND '01/02/2021'
+group by
+university,
+carrer,
+inscription_date,
+first_name,
+last_name,
+gender,
+age,
+location,
+email
+
 
