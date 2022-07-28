7cc105037fd79e6efcbbbd35fbe52db4f24981ea
nadiamc <nadia_827@hotmail.com>
2022-07-28 16:21:04 -0300

    se arregla el formato del código

--- airflow/dags/dag_univ_nacional_de_jujuy.py
+++ airflow/dags/dag_univ_nacional_de_jujuy.py
@@ -1,50 +1,52 @@
+import logging
 from datetime import datetime, timedelta
+
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-import logging
 
-
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(message)s",
-    datefmt="%Y-%m-%d"
-)
+logging.basicConfig(format="%(asctime)s - %(name)s - %(message)s", datefmt="%Y-%m-%d")
 
 args = {
-	'description': 'obtener información sobre Univ. Nacional de Jujuy',
-    'start_date': datetime(2022, 7, 30),
-    'schedule_interval' : timedelta(hours=1)
+    "description": "obtener información sobre Univ. Nacional de Jujuy",
+    "start_date": datetime(2022, 7, 30),
+    "schedule_interval": timedelta(hours=1),
 }
 
-dag = DAG('dag_univ_nac_de_jujuy',default_args=args)
+dag = DAG("dag_univ_nac_de_jujuy", default_args=args)
+
 
 def extraer_datos_univ_nac_de_jujuy():
     pass
 
+
 def transformar_datos_univ_nac_de_jujuy():
     pass
+
 
 def cargar_datos_univ_nac_de_jujuy():
     pass
 
 
 extraer_datos_univ_nac_de_jujuy = PythonOperator(
-    task_id = 'extraer_datos',
-    retries = 5,
-    retry_delay = timedelta(minutes = 1),
-    python_callable = extraer_datos_univ_nac_de_jujuy,
-    dag=dag
+    task_id="extraer_datos",
+    retries=5,
+    retry_delay=timedelta(minutes=1),
+    python_callable=extraer_datos_univ_nac_de_jujuy,
+    dag=dag,
 )
 
 transformar_datos_univ_nac_de_jujuy = PythonOperator(
-    task_id = 'transformar_datos',
-    python_callable = transformar_datos_univ_nac_de_jujuy,
-    dag=dag
+    task_id="transformar_datos",
+    python_callable=transformar_datos_univ_nac_de_jujuy,
+    dag=dag,
 )
 
 cargar_datos_univ_nac_de_jujuy = PythonOperator(
-    task_id = "cargar_datos",
-    python_callable = cargar_datos_univ_nac_de_jujuy,
-    dag=dag
+    task_id="cargar_datos", python_callable=cargar_datos_univ_nac_de_jujuy, dag=dag
 )
 
-extraer_datos_univ_nac_de_jujuy << transformar_datos_univ_nac_de_jujuy << cargar_datos_univ_nac_de_jujuy
+(
+    extraer_datos_univ_nac_de_jujuy
+    << transformar_datos_univ_nac_de_jujuy
+    << cargar_datos_univ_nac_de_jujuy
+)
