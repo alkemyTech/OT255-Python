d26f2d7b4c8902e0685479265701ed9b880c74c9
Juan Cruz <ferreyra.juancruz95@gmail.com>
2022-08-01 20:25:57 -0300

    UC-UBA add s3 load task

--- Airflow/DAGs/dag_uba.py
+++ Airflow/DAGs/dag_uba.py
@@ -1,7 +1,9 @@
 import logging
 from datetime import datetime, timedelta
 
-from airflow.operators.python import PythonOperator, PythonVirtualenvOperator
+from airflow.operators.python import PythonVirtualenvOperator
+from airflow.providers.amazon.aws.transfers.local_to_s3 import \
+    LocalFilesystemToS3Operator
 from include.process_univ import process_univ
 from include.query_univ import query_univ
 
@@ -26,11 +28,6 @@ stream_handler.setFormatter(formatter)
 
 logger.addHandler(file_handler)
 
-
-def _pass():
-    pass
-
-
 # -- DAG --
 # set up the DAG for the current university
 with DAG(
@@ -54,12 +51,15 @@ with DAG(
         task_id=f"task_process_{univ_name}",
         python_callable=process_univ,
         op_args=[univ_name],
-        requirements=["pandas"]
+        requirements=["pandas"],
     )
     # third task: upload resulting object to amazon s3
-    task_load_univ = PythonOperator(
+    task_load_univ = LocalFilesystemToS3Operator(
         task_id=f"task_load_{univ_name}",
-        python_callable=_pass
+        filename=f"./files/modified/g255_{univ_name}.csv",
+        dest_key=f"g255_{univ_name}.csv",
+        dest_bucket="cohorte-julio-8972766c",
+        replace=True,
     )
 
 task_query_univ >> task_process_univ >> task_load_univ
