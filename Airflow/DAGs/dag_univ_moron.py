f2b1ccbe85c8dfd466b56096579d1ef2ad970e0c
maxnelsonlopez <manlopezre@unal.edu.co>
2022-08-02 14:24:58 -0500

    Añadido log
    Añadidas las funciones de log a cada dag

--- Airflow/DAGs/dag_univ_moron.py
+++ Airflow/DAGs/dag_univ_moron.py
@@ -1,8 +1,20 @@
 from datetime import datetime, timedelta
+import logging
 
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(message)s",
+    datefmt="%Y-%m-%d",
+)
+
+# Create custom logger, add console handler
+logger = logging.getLogger(__name__)
+console_handler = logging.StreamHandler()
+logger.addHandler(console_handler)
 
 # -- TASKS --
 # development of tasks that take part in the DAG
