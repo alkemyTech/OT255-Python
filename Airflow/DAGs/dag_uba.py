f9a6ed1b586895db72e8fc7a9ff2e4ca2e156a05
Juan Cruz <ferreyra.juancruz95@gmail.com>
2022-08-02 15:29:06 -0300

    UC-UBA fix S3 connection issues

--- Airflow/DAGs/dag_uba.py
+++ Airflow/DAGs/dag_uba.py
@@ -1,13 +1,13 @@
 import logging
 from datetime import datetime, timedelta
 
+from airflow import DAG
 from airflow.operators.python import PythonVirtualenvOperator
 from airflow.providers.amazon.aws.transfers.local_to_s3 import \
     LocalFilesystemToS3Operator
+
 from include.process_univ import process_univ
 from include.query_univ import query_univ
-
-from airflow import DAG
 
 # -- INITIAL CONFIG --
 # set the name of the university as a variable to simplify code reuse.
@@ -51,13 +51,13 @@ with DAG(
         task_id=f"task_process_{univ_name}",
         python_callable=process_univ,
         op_args=[univ_name],
-        requirements=["pandas"],
+        requirements=["pandas"]
     )
     # third task: upload resulting object to amazon s3
     task_load_univ = LocalFilesystemToS3Operator(
         task_id=f"task_load_{univ_name}",
-        aws_conn_id="s3_conn_alkemy",
         filename=f"./files/modified/g255_{univ_name}.csv",
+        aws_conn_id="s3_alkemy",
         dest_key=f"g255_{univ_name}.csv",
         dest_bucket="cohorte-julio-8972766c",
         replace=True,
