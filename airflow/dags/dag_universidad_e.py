458d320ab9841333cbe20a93d716b588039ae1ac
LucasFerSantos <fernandezsantosclaudiolucas@gmail.com>
2022-08-03 12:09:05 -0300

    load to s3

--- airflow/dags/dag_universidad_e.py
+++ airflow/dags/dag_universidad_e.py
@@ -3,10 +3,13 @@ from datetime import  timedelta, datetime
 from airflow.operators.dummy import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.hooks.postgres_hook import PostgresHook
+from airflow.providers.amazon.aws.transfers.local_to_s3 import LocalFilesystemToS3Operator
 import logging
 from src.py_functions.extract_e import uInter
 from src.py_functions.extract_e  import uPampa
 from src.py_functions.clean_e import cleaningData
+from src.py_functions.cargas3e import cargar_inter
+from src.py_functions.cargas3e import cargar_pampa
 
 
 logger = logging.getLogger('logger')
@@ -29,18 +32,37 @@ with DAG(
     default_args=default_args_dag
 ) as dag:
     consulta_Interamericana = PythonOperator(
-        task_id='consultaSQL_UNMoron',
+        task_id='consultaSQL_inter',
         python_callable=uInter        
         )
     consulta_Pampa = PythonOperator(
-        task_id='consultaSQL_UNRC',
+        task_id='consultaSQL_pampa',
         python_callable=uPampa        
         )
     procesamientoPandas = PythonOperator(
         task_id='procesamientoPandas',
         python_callable=cleaningData        
         )
-    cargaS3 = DummyOperator(task_id='cargaS3')
+    
+    
+    carga_pampa =  LocalFilesystemToS3Operator(
+        task_id='carga pmpa',
+        filename="./files/modified/OT255_universidad_lapampa.txt",
+        aws_conn_id="s3_conn",
+        dest_key="OT255_universidad_lapampa.txt",
+        dest_bucket="*****",
+        replace=True
+        
+    )
+    carga_inter =  LocalFilesystemToS3Operator(
+        task_id='carga inter',
+        filename="./files/modified/OT255_universidad_interamericana.txt",
+        aws_conn_id="s3_conn",
+        dest_key="OT255_universidad_interamericana.txt",
+        dest_bucket="*****",
+        replace=True
+        
+    )
      
 
-    [consulta_Interamericana, consulta_Pampa ] >> procesamientoPandas >> cargaS3
+    [consulta_Interamericana, consulta_Pampa ] >> procesamientoPandas >> [carga_inter, carga_pampa ]
