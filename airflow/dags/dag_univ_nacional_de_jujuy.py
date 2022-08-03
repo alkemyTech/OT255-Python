48ad91b0aebe1d156e22813e8a00277f6d546739
nadiamc <nadia_827@hotmail.com>
2022-08-03 01:17:30 -0300

    extracción de datos y exportación a archivo .csv

--- airflow/dags/dag_univ_nacional_de_jujuy.py
+++ airflow/dags/dag_univ_nacional_de_jujuy.py
@@ -1,8 +1,10 @@
+import csv
 import logging
 from datetime import datetime, timedelta
 
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 logging.basicConfig(format="%(asctime)s - %(name)s - %(message)s", datefmt="%Y-%m-%d")
 
@@ -16,7 +18,46 @@ dag = DAG("dag_univ_nac_de_jujuy", default_args=args)
 
 
 def extraer_datos_univ_nac_de_jujuy():
-    pass
+    cursor_contenido = extraer_info_de_universidad(
+        "jujuy_utn", "universidad nacional de jujuy"
+    )
+    escribir_en_csv("universidad nacional de jujuy", cursor_contenido)
+
+
+def extraer_info_de_universidad(nombre_tabla, universidad):
+    consulta_sql = (
+        "select * from " + nombre_tabla + "  where university = '" + universidad + "'"
+    )
+    hook = PostgresHook(postgres_conn_id="postgres_extraccion")
+    conexion = hook.get_conn()
+    cursor_contenido = conexion.cursor()
+    cursor_contenido.execute(consulta_sql)
+    return cursor_contenido
+
+
+def obtener_nombres_de_columnas(cursor_contenido):
+    lista_de_nombres = [columna[0] for columna in cursor_contenido.description]
+    nombres_separados_por_comas = ",".join(lista_de_nombres)
+    return nombres_separados_por_comas
+
+
+def escribir_en_csv(universidad, cursor_contenido):
+    universidad = espacios_por_guiones(universidad)
+    archivo_csv = open(
+        "/home/nadia/airflow/dags/proyecto_1/csv/" + universidad + ".csv", "a"
+    )
+    nombres_de_columnas = obtener_nombres_de_columnas(cursor_contenido) + '\n'
+    archivo_csv.write(nombres_de_columnas)
+    csv.writer(archivo_csv).writerows(cursor_contenido)
+    archivo_csv.close()
+
+
+def espacios_por_guiones(frase_con_espacios):
+    for indice in range(len(frase_con_espacios)):
+        caracter = frase_con_espacios[indice]
+        if caracter == " ":
+            frase_con_espacios = frase_con_espacios.replace(" ", "_")
+    return frase_con_espacios
 
 
 def transformar_datos_univ_nac_de_jujuy():
--- airflow/dags/dag_univ_palermo.py
+++ airflow/dags/dag_univ_palermo.py
@@ -1,8 +1,10 @@
+import csv
 import logging
 from datetime import datetime, timedelta
 
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 logging.basicConfig(format="%(asctime)s - %(name)s - %(message)s", datefmt="%Y-%m-%d")
 
@@ -16,7 +18,45 @@ dag = DAG("dag_univ_palermo", default_args=args)
 
 
 def extraer_datos_univ_palermo():
-    pass
+    cursor_contenido = extraer_info_de_universidad(
+        "palermo_tres_de_febrero", "_universidad_de_palermo"
+    )
+    escribir_en_csv("_universidad_de_palermo", cursor_contenido)
+
+
+def extraer_info_de_universidad(nombre_tabla, universidad):
+    consulta_sql = (
+        "select * from " + nombre_tabla + "  where universidad = '" + universidad + "'"
+    )
+    hook = PostgresHook(postgres_conn_id="postgres_extraccion")
+    conexion = hook.get_conn()
+    cursor_contenido = conexion.cursor()
+    cursor_contenido.execute(consulta_sql)
+    return cursor_contenido
+
+
+def obtener_nombres_de_columnas(cursor_contenido):
+    lista_de_nombres = [columna[0] for columna in cursor_contenido.description]
+    nombres_separados_por_comas = ",".join(lista_de_nombres)
+    return nombres_separados_por_comas
+
+
+def escribir_en_csv(universidad, cursor_contenido):
+    universidad = quitar_primer_caracter(universidad)
+    archivo_csv = open(
+        "/home/nadia/airflow/dags/proyecto_1/csv/" + universidad + ".csv", "a"
+    )
+    nombres_de_columnas = obtener_nombres_de_columnas(cursor_contenido) + "\n"
+    archivo_csv.write(nombres_de_columnas)
+    csv.writer(archivo_csv).writerows(cursor_contenido)
+    archivo_csv.close()
+
+
+def quitar_primer_caracter(frase):
+    lista_de_palabras = frase.split("_")
+    lista_de_palabras.pop(0)
+    frase = "_".join(lista_de_palabras)
+    return frase
 
 
 def transformar_datos_univ_palermo():
