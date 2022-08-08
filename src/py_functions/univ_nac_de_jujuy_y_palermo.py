03c87e7e8713b04dbc13efc0f8e6ff22a66bb755
nadiamc <nadia_827@hotmail.com>
2022-08-07 23:38:50 -0300

    se arreglan las rutas la modularización de las funciones

--- src/py_functions/univ_nac_de_jujuy_y_palermo.py
+++ src/py_functions/univ_nac_de_jujuy_y_palermo.py
@@ -1,77 +1,66 @@
 import csv
+import pathlib
 
 import pandas
+
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 
 def extraer_datos_univ_nac_de_jujuy():
-    nombre_universidad = "universidad nacional de jujuy"
-    cursor_contenido = extraer_datos_con_archivo_sql(nombre_universidad)
-    escribir_en_csv(nombre_universidad, cursor_contenido)
-
-
-def extraer_datos_univ_nac_de_jujuy_v2():
-    cursor_contenido = extraer_info_de_universidad(
-        "jujuy_utn", "universidad nacional de jujuy"
-    )
-    escribir_en_csv("universidad nacional de jujuy", cursor_contenido)
+    nombre_universidad = "univ_nacional_de_jujuy"
+    extraer_y_escribir_datos_en_csv(nombre_universidad)
 
 
 def extraer_datos_univ_palermo():
-    nombre_universidad = "universidad de palermo"
-    cursor_contenido = extraer_datos_con_archivo_sql(nombre_universidad)
-    escribir_en_csv(nombre_universidad, cursor_contenido)
+    nombre_universidad = "univ_de_palermo"
+    extraer_y_escribir_datos_en_csv(nombre_universidad)
 
 
-def extraer_datos_con_archivo_sql(universidad):
-    universidad = espacios_por_guiones(universidad)
-    universidad = universidad.replace("universidad", "univ")
-    ruta = "/home/nadia/airflow/src/sql/" + universidad + ".sql"
-    archivo_sql = open(ruta, "r")
-    consulta_sql = archivo_sql.read()
-    cursor_contenido = conectar_y_obtener_contenido(consulta_sql)
-    return cursor_contenido
+def transformar_datos_univ_nac_de_jujuy():
+    universidad = "univ_nacional_de_jujuy"
+    normalizar_y_escribir_datos_en_txt(universidad)
 
 
-def extraer_info_de_universidad(nombre_tabla, universidad):
-    condicion = "university = '" + universidad + "'"
-    if "palermo" in universidad:
-        condicion = "universidad = '" + universidad + "'"
-    consulta_sql = "select * from " + nombre_tabla + " where " + condicion
-    cursor_contenido = conectar_y_obtener_contenido(consulta_sql)
-    return cursor_contenido
+def transformar_datos_univ_palermo():
+    universidad = "univ_de_palermo"
+    normalizar_y_escribir_datos_en_txt(universidad)
 
 
-def conectar_y_obtener_contenido(consulta_sql):
-    hook = PostgresHook(postgres_conn_id="postgres_extraccion")
-    conexion = hook.get_conn()
+def cargar_datos_univ_nac_de_jujuy():
+    pass
+
+
+def cargar_datos_univ_palermo():
+    pass
+
+
+def extraer_y_escribir_datos_en_csv(universidad):
+    ruta_sql = pathlib.Path.cwd() / "src" / "sql" / (universidad + ".sql")
+    consulta_sql = open(ruta_sql, "r").read()
+    conexion = PostgresHook(postgres_conn_id="postgres_extraccion").get_conn()
     cursor_contenido = conexion.cursor()
     cursor_contenido.execute(consulta_sql)
-    return cursor_contenido
-
-
-def obtener_nombres_de_columnas(cursor_contenido):
+    ruta_csv = pathlib.Path.cwd() / "src" / "csv" / (universidad + ".csv")
+    archivo_csv = open(ruta_csv, "a")
     lista_de_nombres = [columna[0] for columna in cursor_contenido.description]
-    nombres_separados_por_comas = ",".join(lista_de_nombres)
-    return nombres_separados_por_comas
-
-
-def escribir_en_csv(universidad, cursor_contenido):
-    universidad = espacios_por_guiones(universidad)
-    universidad = universidad.lstrip("_")
-    archivo_csv = open("/home/nadia/airflow/src/csv/" + universidad + ".csv", "a")
-    nombres_de_columnas = obtener_nombres_de_columnas(cursor_contenido) + "\n"
-    archivo_csv.write(nombres_de_columnas)
+    nombres_separados_por_comas = ",".join(lista_de_nombres) + "\n"
+    archivo_csv.write(nombres_separados_por_comas)
     csv.writer(archivo_csv).writerows(cursor_contenido)
     archivo_csv.close()
 
 
-def espacios_por_guiones(frase_con_espacios):
-    for indice in range(len(frase_con_espacios)):
-        caracter = frase_con_espacios[indice]
-        if caracter == " ":
-            frase_con_espacios = frase_con_espacios.replace(" ", "_")
-    return frase_con_espacios
+def normalizar_y_escribir_datos_en_txt(universidad):
+    ruta = pathlib.Path.cwd() / "src" / "csv" / (universidad + ".csv")
+    df_uni = pandas.read_csv(ruta)
+    df_uni.age = df_uni.age.apply(int)
+    df_uni = df_uni.applymap(quitar_guiones_y_espacios_sobrantes)
+    df_uni.gender = df_uni.gender.apply(arreglar_valor_de_genero)
+    for indice, localidad in df_uni.location.items():
+        codigo_postal = obtener_codigo_postal_segun_localidad(localidad)
+        df_uni.at[indice, "postal_code"] = codigo_postal
+    datos_como_string = df_uni.to_string(index=False)
+    ruta_txt = pathlib.Path.cwd() / "src" / "txt" / (universidad + ".txt")
+    open(ruta_txt, "a").write(datos_como_string)
 
 
 def quitar_guiones_y_espacios_sobrantes(valor):
@@ -89,68 +78,14 @@ def arreglar_valor_de_genero(valor):
     return valor
 
 
-def normalizar_universidad(nombre_universidad):
-    nombre_universidad = espacios_por_guiones(nombre_universidad)
-    ruta = "/home/nadia/airflow/src/csv/" + nombre_universidad + ".csv"
-    df_universidad = pandas.read_csv(ruta)
-    df_universidad = corregir_formato(df_universidad)
-    return df_universidad
-
-
-def corregir_formato(df_universidad):
-    df_universidad["age"] = df_universidad["age"].apply(int)
-    df_universidad = df_universidad.applymap(quitar_guiones_y_espacios_sobrantes)
-    df_universidad["gender"] = df_universidad["gender"].apply(arreglar_valor_de_genero)
-    return df_universidad
-
-
-def normalizar_codigos_postales():
-    ruta = "/home/nadia/airflow/src/csv/codigos_postales.csv"
-    df_codigos_postales = pandas.read_csv(ruta)
+def obtener_codigo_postal_segun_localidad(localidad):
+    ruta_csv = pathlib.Path.cwd() / "src" / "csv" / ("codigos_postales.csv")
+    df_codigos_postales = pandas.read_csv(ruta_csv)
     df_codigos_postales = df_codigos_postales.applymap(
         quitar_guiones_y_espacios_sobrantes
     )
-    return df_codigos_postales
-
-
-def obtener_codigo_postal(df_codigos_postales, localidad):
+    df_codigos_postales = df_codigos_postales.drop_duplicates()
     condicion = df_codigos_postales.localidad.str.contains(localidad)
     df_codigos_postales = df_codigos_postales[condicion]
     codigo_postal = df_codigos_postales.codigo_postal.iloc[0]
     return codigo_postal
-
-
-def corregir_codigos_postales(df_universidad):
-    df_codigos_postales = normalizar_codigos_postales()
-    df_codigos_postales = df_codigos_postales.drop_duplicates()
-    for indice, localidad in df_universidad.location.items():
-        codigo_postal = obtener_codigo_postal(df_codigos_postales, localidad)
-        df_universidad.at[indice, "postal_code"] = codigo_postal
-    return df_universidad
-
-
-def escribir_en_archivo_txt(df_universidad, iniciales_uni):
-    ruta = "/home/nadia/airflow/src/txt/datos_normalizados_" + iniciales_uni + ".txt"
-    archivo_txt = open(ruta, "a")
-    datos_como_string = df_universidad.to_string(index=False)
-    archivo_txt.write(datos_como_string)
-
-
-def transformar_datos_univ_nac_de_jujuy():
-    df_univ_nac_de_jujuy = normalizar_universidad("universidad nacional de jujuy")
-    df_univ_nac_de_jujuy = corregir_codigos_postales(df_univ_nac_de_jujuy)
-    escribir_en_archivo_txt(df_univ_nac_de_jujuy, "UNJ")
-
-
-def transformar_datos_univ_palermo():
-    df_univ_de_palermo = normalizar_universidad("universidad de palermo")
-    df_univ_de_palermo = corregir_codigos_postales(df_univ_de_palermo)
-    escribir_en_archivo_txt(df_univ_de_palermo, "UP")
-
-
-def cargar_datos_univ_nac_de_jujuy():
-    pass
-
-
-def cargar_datos_univ_palermo():
-    pass
