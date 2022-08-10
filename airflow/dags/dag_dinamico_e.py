99f4f0a67bf323e338ce6833d28f0cb47ff08103
LucasFerSantos <fernandezsantosclaudiolucas@gmail.com>
2022-08-10 16:57:35 -0300

    cambios en dag dinamico

--- airflow/dags/dag_dinamico_e.py
+++ airflow/dags/dag_dinamico_e.py
@@ -2,7 +2,7 @@ from airflow import DAG
 import dagfactory
 
 
-dag_factory = dagfactory.DagFactory("/home/user/airflow/dags/config_e.yml")
+dag_factory = dagfactory.DagFactory("/usr/local//airflow/dags/config_e.yml")
 
 dag_factory.clean_dags(globals())
 dag_factory.generate_dags(globals())
