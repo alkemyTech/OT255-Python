b4a54ee617962081395c4643eeab91363d6ae4b7
Exhn <100232788+Exhn@users.noreply.github.com>
2022-08-21 19:48:52 -0300

    Dynamic dags with full process universities B

--- src/py_functions/pandas_comahue.py
+++ src/py_functions/pandas_comahue.py
@@ -1,20 +1,18 @@
 """Python function for pandas processing for universidad nacional del comahue"""
 import pandas as pd
-from dotenv import load_dotenv
+from pathlib import Path
 
-# Loading variables from dotenv
-load_dotenv()
+filePath = Path(__file__).parent.parent.parent / "destFolder" / "Universidad Nacional del Comahue.csv"
+postalPath = Path(__file__).parent.parent.parent / "postal_codes" / "codigos_postales.csv"
+destPath = Path(__file__).parent.parent.parent / "destFolder" / "universidad_del_comahue.txt"
 
-destFolder = os.getenv('destFolder')
-postal_code = os.getenv('postal_code')
 
 def pandas_processing_comahue():
     """Python function for pandas processing for universidad nacional del comahue"""
-    with open(f'{destFolder}/universidad_del_comahue.csv', 'r') as f:
+    with open(filePath, 'r') as f:
         df = pd.read_csv(f, encoding='latin-1')
         df = df.astype({'age': 'int'})
         df = df.astype({'postal_code': 'str'})
-        df = df.drop('location', axis=1)
         df['university'] = df['university'].str.lower()
         df['career'] = df['career'].str.lower()
         df['first_name'] = df['first_name'].str.lower()
@@ -23,11 +21,11 @@ def pandas_processing_comahue():
         df.loc[df['gender'] == 'F', 'gender'] = 'female'
         df['email'] = df['email'].str.lower()
 
-    with open(f'{postal_code}/codigos_postales.csv', 'r') as cp:
+    with open(postalPath, 'r') as cp:
         dfpostal_code = pd.read_csv(cp, encoding='latin-1')
         dfpostal_code.rename(columns={'codigo_postal': 'postal_code', 'localidad': 'location'}, inplace=True)
         dfpostal_code['location'] = dfpostal_code['location'].str.lower()
         dfpostal_code = dfpostal_code.astype({'postal_code': 'str'})
 
     df = df.merge(dfpostal_code, on='postal_code', how='left')
-    df.to_csv(r'universidad_del_comahue.txt', header=None, index=None, sep=' ', mode='a')
+    df.to_csv(destPath, header=None, index=None, sep=' ', mode='a')
--- src/py_functions/pandas_salvador.py
+++ src/py_functions/pandas_salvador.py
@@ -1,19 +1,17 @@
 """Python function for pandas processing for universidad del salvador"""
 import pandas as pd
-from dotenv import load_dotenv
+from pathlib import Path
 
-# Loading variables from dotenv
-load_dotenv()
+filePath = Path(__file__).parent.parent.parent / "destFolder" / "Universidad del Salvador.csv"
+postalPath = Path(__file__).parent.parent.parent / "postal_codes" / "codigos_postales.csv"
+destPath = Path(__file__).parent.parent.parent / "destFolder" / "universidad_del_salvador.txt"
 
-destFolder = os.getenv('destFolder')
-postal_code = os.getenv('postal_code')
 
 def pandas_processing_salvador():
     """Python function for pandas processing for universidad del salvador"""
-    with open(f'{destFolder}/universidad_del_salvador.csv', 'r') as f:
+    with open(filePath, 'r') as f:
         df = pd.read_csv(f, encoding='latin-1')
         df = df.astype({'age': 'int'})
-        df = df.drop('postal_code', axis=1)
         df['university'] = df['university'].str.replace('_', ' ').str.lower().str.strip()
         df['career'] = df['career'].str.replace('_', ' ').str.lower().str.strip()
         df['first_name'] = df['first_name'].str.replace('_', ' ').str.lower()
@@ -23,11 +21,11 @@ def pandas_processing_salvador():
         df['location'] = df['location'].str.lower()
         df['email'] = df['email'].str.replace('_', ' ').str.lower()
 
-    with open(f'{postal_code}/codigos_postales.csv', 'r') as cp:
+    with open(postalPath, 'r') as cp:
         dfpostal_code = pd.read_csv(cp, encoding='latin-1')
         dfpostal_code.rename(columns={'codigo_postal': 'postal_code', 'localidad': 'location'}, inplace=True)
         dfpostal_code = dfpostal_code.astype({'postal_code': 'str'})
         dfpostal_code['location'] = dfpostal_code['location'].str.lower()
 
     df = df.merge(dfpostal_code, on='location', how='left')
-    df.to_csv(r'universidad_del_salvador.txt', header=None, index=None, sep=' ', mode='a')
+    df.to_csv(destPath, header=None, index=None, sep=' ', mode='a')
