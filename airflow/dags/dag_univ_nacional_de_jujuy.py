4518aa8beb1f996e0f10926570ed8cd99468a40a
nadiamc <nadia_827@hotmail.com>
2022-08-26 08:31:57 -0300

    DAG universidades C
    * se suben las correcciones pedidas
    
    * se suben las correcciones pedidas
    
    * avances en carga de datos
    
    * s3 y consulta sql de UP
    
    * se suben los dags dinámicos y se corrigen los operators de carga
    
    Co-authored-by: noemi <noemi@debian-BULLSEYE-live-builder-AMD64>

--- airflow/dags/dag_univ_nacional_de_jujuy.py
+++ airflow/dags/dag_univ_nacional_de_jujuy.py
@@ -3,6 +3,8 @@ from datetime import datetime, timedelta
 
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.providers.amazon.aws.transfers.local_to_s3 import \
+    LocalFilesystemToS3Operator
 
 logging.basicConfig(format="%(asctime)s - %(name)s - %(message)s", datefmt="%Y-%m-%d")
 
@@ -15,35 +17,33 @@ args = {
 dag = DAG("dag_univ_nac_de_jujuy", default_args=args)
 
 
-def extraer_datos_univ_nac_de_jujuy():
-    pass
-
-
-def transformar_datos_univ_nac_de_jujuy():
-    pass
-
-
-def cargar_datos_univ_nac_de_jujuy():
-    pass
-
-
+logging.info("Se extraen datos.")
 extraer_datos_univ_nac_de_jujuy = PythonOperator(
     task_id="extraer_datos",
     retries=5,
     retry_delay=timedelta(minutes=1),
-    python_callable=extraer_datos_univ_nac_de_jujuy,
+    python_callable=extraer_y_escribir_datos_en_csv("univ_nacional_de_jujuy"),
     dag=dag,
 )
 
+logging.info("Se transforman datos.")
 transformar_datos_univ_nac_de_jujuy = PythonOperator(
     task_id="transformar_datos",
-    python_callable=transformar_datos_univ_nac_de_jujuy,
+    python_callable=normalizar_y_escribir_datos_en_txt("univ_nacional_de_jujuy"),
     dag=dag,
 )
 
-cargar_datos_univ_nac_de_jujuy = PythonOperator(
-    task_id="cargar_datos", python_callable=cargar_datos_univ_nac_de_jujuy, dag=dag
+logging.info("Se cargan datos.")
+cargar_datos_univ_nac_de_jujuy = LocalFilesystemToS3Operator(
+    task_id="cargar_datos_UNJ",
+    filename=pathlib.Path.cwd() / "src" / "txt" / "univ_nacional_de_jujuy.txt",
+    aws_conn_id="carga_to_S3",
+    dest_key="univ_nacional_de_jujuy.txt",
+    dest_bucket="cohorte-julio-8972766c",
+    replace=True,
+    dag=dag,
 )
+
 
 (
     extraer_datos_univ_nac_de_jujuy
--- airflow/dags/dag_univ_palermo.py
+++ airflow/dags/dag_univ_palermo.py
@@ -3,6 +3,8 @@ from datetime import datetime, timedelta
 
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.providers.amazon.aws.transfers.local_to_s3 import \
+    LocalFilesystemToS3Operator
 
 logging.basicConfig(format="%(asctime)s - %(name)s - %(message)s", datefmt="%Y-%m-%d")
 
@@ -15,33 +17,34 @@ args = {
 dag = DAG("dag_univ_palermo", default_args=args)
 
 
-def extraer_datos_univ_palermo():
-    pass
-
-
-def transformar_datos_univ_palermo():
-    pass
-
-
-def cargar_datos_univ_palermo():
-    pass
-
-
+logging.info("Se extraen datos.")
 extraer_datos_univ_palermo = PythonOperator(
     task_id="extraer_datos",
     retries=5,
     retry_delay=timedelta(minutes=1),
-    python_callable=extraer_datos_univ_palermo,
+    python_callable=extraer_y_escribir_datos_en_csv("univ_de_palermo.sql"),
     dag=dag,
 )
 
+
+logging.info("Se transforman datos.")
 transformar_datos_univ_palermo = PythonOperator(
-    task_id="transformar_datos", python_callable=transformar_datos_univ_palermo, dag=dag
+    task_id="transformar_datos",
+    python_callable=normalizar_y_escribir_datos_en_txt("univ_de_palermo.sql"),
+    dag=dag,
 )
 
-cargar_datos_univ_palermo = PythonOperator(
-    task_id="cargar_datos", python_callable=cargar_datos_univ_palermo, dag=dag
+logging.info("Se cargan datos.")
+cargar_datos_univ_palermo = LocalFilesystemToS3Operator(
+    task_id="cargar_datos_UP",
+    filename=pathlib.Path.cwd() / "src" / "txt" / "univ_de_palermo.txt",
+    aws_conn_id="carga_to_S3",
+    dest_key="univ_de_palermo.txt",
+    dest_bucket="cohorte-julio-8972766c",
+    replace=True,
+    dag=dag,
 )
+
 
 (
     extraer_datos_univ_palermo
